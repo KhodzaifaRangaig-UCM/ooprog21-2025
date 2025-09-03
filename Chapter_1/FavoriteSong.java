118dc19794e874c1df58069d47e71ec4e58e44f3
KhodzaifaRangaig-UCM <imomama97@gmail.com>
2025-09-03 08:32:17 +0800

    Create FavoriteSong.java

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,9 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("There's no Easy Way");
+        System.out.println("Every Woman in the World");
+        System.out.println("Mahal parin kita");
+        System.out.println("Iniibig Kita");
+    
+    }
+}
